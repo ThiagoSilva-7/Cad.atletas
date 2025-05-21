413c618d8784e50c2b16e2964aa86bed2c48fe3f
ThiagoSilva-7 <thiagomarquesjp04@hotmail.com>
2025-05-20 23:38:22 -0300

    Delete garmin_upload.py

--- garmin_upload.py
+++
@@ -1,9 +0,0 @@
-
-import streamlit as st
-
-def pagina_garmin_upload(atleta_id):
-    st.header("📤 Upload de arquivos Garmin (.tcx ou .fit)")
-    uploaded_file = st.file_uploader("Enviar arquivo TCX ou FIT", type=["tcx", "fit"])
-    if uploaded_file:
-        st.success(f"Arquivo '{uploaded_file.name}' enviado com sucesso!")
-        st.write("Esta funcionalidade será estendida para leitura e importação dos dados do arquivo.")
